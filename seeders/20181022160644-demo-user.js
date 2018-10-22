72f077334f134a0e2aff8b4791bbc91253888b24
Deivid Veloso <veloso.deivid@gmail.com>
2018-10-22 13:32:31 -0300

    add seeders

---
+++ seeders/20181022160644-demo-user.js
@@ -0,0 +1,38 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          firstName: "Deivid",
+          lastName: "Veloso",
+          email: "veloso.deivid@gmail.com"
+        }
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete("users", null, {});
+  }
+};
--- services/users.js
+++ services/users.js
@@ -1,7 +1,7 @@
 const User = require("../models/user");
 function getUsers(req, res, next) {
   User.findAll().then(user => {
-    console.log(user);
+    return res.status(200).json(user);
   });
 }
 
